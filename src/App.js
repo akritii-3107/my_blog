e9ab69d4a1047520bf071f63d572c3df38282c0b
akritii-3107 <107861050+akritii-3107@users.noreply.github.com>
2023-03-22 01:11:05 +0530

    commit new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticleListPage from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar>
+      </NavBar>
+      <div id="page-body">
+        <Routes>
+
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/about" element={<AboutPage/>}></Route>
+        <Route path="/articles" element={<ArticleListPage/>}></Route>
+        <Route path="/articles/:articleId" element={<ArticlePage/>}></Route>
+        </Routes>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,16 @@
+//localhost:3000/articles/learn_node
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+const ArticlePage = () => {
+    const { articleId }  = useParams();
+    const article = articles.find(article => article.name === articleId);
+    return (
+        <>
+        <h1>{article.title}</h1>
+        {article.content.map( (paragraph,i) => (<p key={i}>{paragraph}</p>))}
+        </>
+    )
+
+}
+ 
+export default ArticlePage
