b893ad26289c1e59b40f91d6592daf0645b1d429
akritii-3107 <107861050+akritii-3107@users.noreply.github.com>
2023-03-26 19:20:06 +0530

    commit 4

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,31 @@
-//localhost:3000/articles/learn_node
+import { useState , useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 const ArticlePage = () => {
+    const [articleInfo , setArticleInfo ] = useState ({ upvotes :0,comments:[]});
     const { articleId }  = useParams();
+
+    useEffect (() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, [articleId]);
+
+    const addupvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+    }
+
     const article = articles.find(article => article.name === articleId);
+
     if(!article)
     {
         return <NotFoundPage/>
@@ -12,7 +33,17 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
+        <div className="upvotes-section">
+        <button onClick={addupvote}>Upvote</button>
+        </div>
+        <p>
+            This article has {articleInfo.upvotes} upvote(s) </p>
         {article.content.map( (paragraph,i) => (<p key={i}>{paragraph}</p>))}
+        <AddCommentForm
+         articleName={articleId}
+         onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}>
+         </AddCommentForm>
+        <CommentsList comments={articleInfo.comments}></CommentsList>
         </>
     )
 
--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,6 +1,6 @@
 import articles from "./article-content";
 import ArticlesList from "../components/ArticlesList";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 const ArticleListPage = () => {
     return (
         <>
