5adc04a179774a0917f9836d439f66d2d7de7fc9
akritii-3107 <107861050+akritii-3107@users.noreply.github.com>
2023-05-14 17:23:05 +0530

    new commit - last step

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
+import useUser from "../hooks/useUser";
 
 const AddCommentForm = ({articleName,onArticleUpdated}) => {
     const [name ,setName] = useState('');
     const [commentText,setCommentText] = useState('');
+    const { user } = useUser();
 
     const addComment = async () => {
+    const token = user && await user.getIdToken();
+    const headers = token ? { authtoken : token} : {};
     const response = await axios.post(`/api/articles/${articleName}/comments`,{
             postedBy:name,
             text:commentText
-        });
+        },{headers});
     const updatedArticle = response.data;
     console.log(response.data);
     onArticleUpdated(updatedArticle);
@@ -19,12 +23,9 @@ const AddCommentForm = ({articleName,onArticleUpdated}) => {
     return (
         <div className="add-commet-form">
             <h3>Add a comment</h3> 
+            {user &&  <p>Your are posting as {user.email} </p>}
+            <br></br>
             <label>
-                 Name:
-                  <input type="text" value={name} onChange={e => setName(e.target.value)}/>
-            </label>
-            <label>
-                 Comment:
                   <textarea rows="4" cols="50" value={commentText} onChange={e => setCommentText(e.target.value)}/>
             </label>
             <button onClick={addComment}>Add Comment</button>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -7,13 +7,17 @@ import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
 import useUser from "../hooks/useUser";
 const ArticlePage = () => {
-    const [articleInfo , setArticleInfo ] = useState ({ upvotes :0,comments:[]});
+    const [articleInfo , setArticleInfo ] = useState ({ upvotes :0,comments:[],canUpvote:false});
+    const { canUpvote } =articleInfo;
     const { articleId }  = useParams();
-    const {user, isLoading}=useUser();
+    const {user}=useUser();
 
     useEffect (() => {
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
+            const token = user && await user.getIdToken();
+            const headers = token ? { authtoken : token} : {};
+            const response = await axios.get(`/api/articles/${articleId}`,
+                {headers},);
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
@@ -21,7 +25,10 @@ const ArticlePage = () => {
     }, [articleId]);
 
     const addupvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken : token} : {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`,null,{ headers });
+
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
@@ -36,7 +43,8 @@ const ArticlePage = () => {
         <>
         <h1>{article.title}</h1>
         <div className="upvotes-section">
-         {user ? <button onClick={addupvote}>Upvote</button> : <button>Login to upvote</button>}   
+         {user ? <button onClick={addupvote}>{ canUpvote ? 'Upvote' : 'Already Upvoted'} </button>
+         : <button>Login to upvote</button>}   
         </div>
         <p>
             This article has {articleInfo.upvotes} upvote(s) </p>
